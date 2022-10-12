454538b355ef986fe5ae9015899c5e3bcaa20eeb
Farah M <farah.maria.rahman@gmail.com>
2022-10-12 00:55:23 +0000

    first pass

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,3 +1,5 @@
+const filterByTerm = require("../filterByTerm");
+
 describe("Filter function", () => {
     test("it should filter by a search term (link)", () => {
         const input = [                                 // input
