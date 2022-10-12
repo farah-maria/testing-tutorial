0835a3c36eaa70336e8a1c4eada2e44624737129
Farah M <farah.maria.rahman@gmail.com>
2022-10-12 21:40:55 +0000

    add regular expressions

--- filterByTerm.js
+++ filterByTerm.js
@@ -1,8 +1,9 @@
 
 
 function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i"); // The RegExp constructor creates a regular expression object for matching text with a pattern.
     return inputArr.filter(function(arrayElement) {
-        return arrayElement.url.match(searchTerm);
+        return arrayElement.url.match(regex);
     });
 }
 
